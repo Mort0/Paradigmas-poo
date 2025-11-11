1da1375bac52ed77f05086a9597e2f2a0c60beea
Diego <Diegoascanio13@gmail.com>
2025-11-10 21:16:12 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+/**
+ *
+ * @author Diego
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
